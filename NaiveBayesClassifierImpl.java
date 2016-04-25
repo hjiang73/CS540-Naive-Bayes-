487425c74c4a682358b057dc08c68099cdf2ec99
hjiang73 <jianghan0325@126.com>
2016-04-25 09:11:11 -0500

    first commit

---
+++ NaiveBayesClassifierImpl.java
@@ -0,0 +1,41 @@
+/**
+ * Your implementation of a naive bayes classifier. Please implement all four methods.
+ */
+
+public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
+	/**
+	 * Trains the classifier with the provided training data and vocabulary size
+	 */
+	@Override
+	public void train(Instance[] trainingData, int v) {
+		// Implement		
+	}
+
+	/**
+	 * Returns the prior probability of the label parameter, i.e. P(SPAM) or P(HAM)
+	 */
+	@Override
+	public double p_l(Label label) {
+		// Implement
+		return 0;
+	}
+
+	/**
+	 * Returns the smoothed conditional probability of the word given the label,
+	 * i.e. P(word|SPAM) or P(word|HAM)
+	 */
+	@Override
+	public double p_w_given_l(String word, Label label) {
+		// Implement
+		return 0;
+	}
+	
+	/**
+	 * Classifies an array of words as either SPAM or HAM. 
+	 */
+	@Override
+	public ClassifyResult classify(String[] words) {
+		// Implement
+		return null;
+	}
+}
